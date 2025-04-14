fde0106ab407201530fe17ad7d1f33efec9fbd28
J Alexis <colores.2808.8000@gmail.com>
2025-04-14 17:40:00 -0600

    Agregando TailwindCSS, VueRouter y primeras vistas

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import HomeView from "../views/HomeView.vue";
+
+const routes = [
+    {
+        path: "/",
+        component: LoginView
+    },
+    {
+        path: "/register",
+        component: RegisterView
+    },
+    {
+        path: "/home",
+        component: HomeView
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+    <h2>Registro</h2>
+    
+</template>
